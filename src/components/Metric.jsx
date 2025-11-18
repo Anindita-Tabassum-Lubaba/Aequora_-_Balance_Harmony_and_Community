63746c768f6a1cbc7dbf8221c11992c1a0191cdb
Jannatul Mawa Tahi <jannatulmawatahi@gmail.com>
2025-11-18 03:41:44 -0600

    ...

--- src/components/Metric.jsx
+++
@@ -1,8 +0,0 @@
-export default function Metric({ label, value, tone }) {
-  return (
-    <div className={`metric ${tone ? `metric--${tone}` : ""}`}>
-      <div className="metric__value">{value}</div>
-      <div className="metric__label">{label}</div>
-    </div>
-  );
-}
